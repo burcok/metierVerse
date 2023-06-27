17df3670a2cd1ec228eccde207732261bb1d99bc
Sefa Burak Dereli <burak.dereli@despatchcloud.com>
2023-06-27 17:02:39 +0300

    edit: notification snackbar customization

--- frontend-side/src/App.vue
+++ frontend-side/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Header from './components/headers/Header.vue';
+import notificationSnackbar from './components/modals/notificationSnackbar.vue';
 </script>
 
 <template>
