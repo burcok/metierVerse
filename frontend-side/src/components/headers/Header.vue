20867573a78712e7c5bfb57b543d9dfe040befea
burcokk <burak.dereli@despatchcloud.com>
2023-07-01 16:06:37 +0300

    feat: made more responsive design

--- frontend-side/src/components/headers/Header.vue
+++ frontend-side/src/components/headers/Header.vue
@@ -16,28 +16,34 @@ export default {
 </script>
 <template>
     <nav class="dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-around mx-auto p-4">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between sm:justify-around mx-auto p-4">
             <a href="" class="flex items-center">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-gray-800 dark:text-gray-200">bilet.isgelir</span>
             </a>
-                <div class="flex md:order-2 ml-10" >
-                    <router-link to="/register" class="duration-300 text-gray-100 hover:text-white bg-primary-color hover:bg-blue-600 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-primary-color dark:hover:bg-primary-color">Get started</router-link>
-                    <router-link to="/login" class="duration-300 text-gray-200 hover:text-primary-color dark:hover:text-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Login</router-link>
-                    <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700" aria-controls="navbar-cta" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                    </button>
-                </div>
-                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                    <ul id="navigation" class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
-                        <li>
-                            <router-link to="/" class="duration-300 block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-color md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</router-link>
-                        </li>
-                        <li>
-                            <router-link to="/discover" class="duration-300 block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-color md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Discover</router-link>
-                        </li>
-                    </ul>
-                </div>
+            <div class="flex md:order-2" >
+                <router-link to="/register" class="hidden md:inline-flex duration-300 text-gray-100 hover:text-white bg-primary-color hover:bg-blue-500 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-primary-color dark:hover:bg-primary-color">Get started</router-link>
+                <router-link to="/login" class="hidden md:inline-flex duration-300 text-gray-600 hover:text-primary-color dark:hover:text-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">Login</router-link>
+                <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700" aria-controls="navbar-cta" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+                <ul id="navigation" class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
+                    <li>
+                        <router-link to="/" class="duration-300 block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-color md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/discover" class="duration-300 block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-color md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Discover</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/register" class="md:hidden inline-flex duration-300 text-gray-100 hover:text-white bg-primary-color hover:bg-blue-600 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-primary-color dark:hover:bg-primary-color">Get started</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/login" class="md:hidden inline-flex duration-300 text-gray-200 hover:text-primary-color dark:hover:text-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">Login</router-link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </template>
