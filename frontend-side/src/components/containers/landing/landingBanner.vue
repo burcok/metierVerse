7df39b2fc140d8e2b19d6c939caed6ea53cc8b50
burcokk <burak.dereli@despatchcloud.com>
2023-07-01 16:04:40 +0300

    edit: file locations changed

---
+++ frontend-side/src/components/containers/landing/landingBanner.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="px-10 max-w-screen-xl mx-auto h-[500px] flex items-center justify-center">
+        <div class="grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 text-black">
+            <div class="p-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, eius.</div>
+            <div class="p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, earum?</div>
+            <div class="p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, corrupti.</div>
+            <div class="p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit, molestias.</div>
+            <div class="p-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam, fuga.</div>
+        </div>
+    </div>
+</template>
